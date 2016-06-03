1f5dcd0be53c03e9e5d56aa3b3c8fef0acbb09c3
Randi Mays <randimays1024@gmail.com>
2016-06-03 16:29:16 -0500

    Updating iterating lesson with for / foreach loop

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -1,15 +1,16 @@
 (function(){
     "use strict";
     var names = ['Randi', 'John', 'Pam', 'Gary'];
-    console.log(names.length);
-    console.log('The first name is: ' + names[0]);
-    console.log('The second name is: ' + names[1]);
-    console.log('The third name is: ' + names[2]);
-    console.log('The fourth name is: ' + names[3]);
+
+    for (var i = 0; i < names.length; i++) {
+    	console.log("The name at index " + i + " is " + names[i]);
+    }
+
+    names.forEach(function(names) {
+    	console.log(names);
+    });
+})();
 	    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-
-
     // TODO: Create a log statement that will log the number of elements in the names array.
 
     // TODO: Create log statements that will print each of the names array elements individually.
-})();

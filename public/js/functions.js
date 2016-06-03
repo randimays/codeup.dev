d4b6590fc74c21310d8cfe2582d42db2b7c4ccbb
Randi Mays <randimays1024@gmail.com>
2016-06-03 11:21:14 -0500

    Adding first function exercises

--- public/js/functions.js
+++ public/js/functions.js
@@ -4,34 +4,33 @@ var myNameIs = 'Randi'; // TODO: Fill in your name here.
 
 // TODO:
 // Create a function called 'sayHello' that takes a parameter 'name'.
+// When called, the function should log a message that says hello from the passed in name.
 
-function sayHello(name) {
-	console.log(myNameIs + ": Hello!");
+function sayHello(myNameIs) {
+	console.log("Hello, " + myNameIs + "!");
 }
 
 sayHello(myNameIs);
 
-// When called, the function should log a message that says hello from the passed in name.
-
 // TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
-
 
 // Don't modify the following line
 // It generates a random number between 1 and 100 and stores it in random
+
 var random = Math.floor((Math.random()*100)+1);
+
+console.log("Random number: " + random);
 
 // TODO:
 // Create a function called 'isOdd' that takes a number as a parameter.
-
-console.log("This is my chosen random number: " + random.toString());
-
-function isOdd(numberToTest) {
-	(numberToTest % 2 === 0 ) ? console.log("This number is even") : console.log("This number is odd");
-}
-
-isOdd(random);
-
 // The function should use the ternary operator to log a message.
 // The log should tell the number passed in and whether it is odd or not.
 
+function isOdd(random) {
+	(random % 2 == 1) ? console.log(random + " is odd!") : console.log(random + " is even!");
+}
+
 // TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
+
+isOdd(random);
+

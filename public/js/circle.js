de944df6d4ccc4d021611c67b183f54a8e2dbcd9
Randi Mays <randimays1024@gmail.com>
2016-06-06 16:53:15 -0500

    Adding math exercises

---
+++ public/js/circle.js
@@ -0,0 +1,35 @@
+(function() {
+	"use strict";
+
+	// create a circle object
+	var circle = {
+		radius: 3,
+		getArea: function () {
+			return (Math.PI) * Math.pow(this.radius, 2);
+		},
+		logInfo: function (doRounding) {
+			var area = this.getArea();
+			if (doRounding) {
+				area = Math.round(area);
+			}
+			console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
+		}
+	};
+
+	// log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+
+    circle.radius = 5;
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();

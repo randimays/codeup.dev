4efed75319e01265448e3c5a439c1c2b05989695
Randi Mays <randimays1024@gmail.com>
2016-06-11 19:47:11 -0500

    updating calculator files

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,25 +1,89 @@
-window.onload = function () {
+"use strict";
 
-	"use strict";
+var operatorButtonsArray = document.getElementsByClassName("operator-btn");
+var operatorCenter = document.getElementById("operator");
+var operandLeft = document.getElementById("leftOperand");
+var operandRight = document.getElementById("rightOperand");
+var decimalButton = document.getElementById("decimalbtn");
 
-	var buttonsArray = document.getElementsByClassName("number-btn");
+function pressNumberButtons (event) {
+	var numberButtonsArray = document.getElementsByClassName("number-btn");
+	for (var i = 0; i < numberButtonsArray.length; i++) {
+		numberButtonsArray[i].addEventListener ("click", numberPushToTop);
+	}
+}
 
-	function pressNumberButtons () {
-		for (var i = 0; i < buttonsArray.length; i++) {
-			buttonsArray[i].addEventListener ("click", pushToTop);
+function pressOperatorButtons (event) {
+	for (var i = 0; i < operatorButtonsArray.length; i++) {
+		operatorButtonsArray[i].addEventListener ("click", operatorPushToTop);
+	}
+}
+
+function pressClearButton (event) {
+	(document.getElementById("clearbtn")).addEventListener ("click", clearData);
+}
+
+function pressEqualsButton (event) {
+	(document.getElementById("equalsbtn")).addEventListener ("click", solveEquation);
+}
+
+function numberPushToTop () {
+	var leftMaxLength = operandLeft.getAttribute("maxlength");
+	var rightMaxLength = operandRight.getAttribute("maxlength");
+	for (var i = 0; i < operatorButtonsArray.length; i++) {
+		operatorButtonsArray[i].removeAttribute("disabled");
+	}
+	if (operatorCenter.value == "") {
+		if (operandLeft.value.length >= leftMaxLength) {
+			operandLeft.value = ((parseInt(operandLeft.value)).toExponential(0)); 
+			operandLeft.value += parseInt(this.value);
+		} else {
+			operandLeft.value += this.value;
 		}
+	} else {
+		if (operandRight.value.length >= rightMaxLength) {
+			operandRight.value = ((parseInt(operandRight.value)).toExponential(0));
+			operandRight.value += parseInt(this.value);
+		} else
+			operandRight.value += this.value;
 	}
+}
 
-	pressNumberButtons();
-	
-	function pushToTop () {
-		document.getElementById("leftOperand").value += this.value;
+
+function checkMaxLength () {
+	operandLeft.getAttribute("maxlength");
+	operandRight.getAttribute("maxlength");
+}
+
+function operatorPushToTop () {
+	operatorCenter.value += this.value;
+}
+
+function clearData () {
+	operandLeft.value = "";
+	operandRight.value = "";
+	operatorCenter.value = "";
+}
+
+function solveEquation () {
+	parseInt(operandLeft.value);
+	parseInt(operandRight.value);
+	var result;
+	if (operatorCenter.value == "+") {
+		result = operandLeft.value + operandRight.value;
+	} else if (operatorCenter.value == "-") {
+		result = operandLeft.value - operandRight.value;
+	} else if (operatorCenter.value == "/") {
+		result = operandLeft.value / operandRight.value;
+	} else if (operatorCenter.value == "*") {
+		result = operandLeft.value * operandRight.value;
 	}
- 
-} 
+		operandLeft.value = result;
+		operandRight.value = "";
+		operatorCenter.value = "";
+}
 
-	// var buttons = [];
-			// var value = buttonsArray[i].getAttribute("value");
-			// var buttons = [];
-			// buttons.push(parseInt(value));
-			// buttons.sort();
+pressNumberButtons(event);
+pressOperatorButtons(event);
+pressClearButton(event);
+pressEqualsButton(event);

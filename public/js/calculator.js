f6b8a567ac645d217044972fd7ffdb2673484c00
Randi Mays <randimays1024@gmail.com>
2016-06-12 12:03:11 -0500

    updating calculator files

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -5,6 +5,8 @@ var operatorCenter = document.getElementById("operator");
 var operandLeft = document.getElementById("leftOperand");
 var operandRight = document.getElementById("rightOperand");
 var decimalButton = document.getElementById("decimalbtn");
+var leftMaxLength = operandLeft.getAttribute("maxlength");
+var rightMaxLength = operandRight.getAttribute("maxlength");
 
 function pressNumberButtons (event) {
 	var numberButtonsArray = document.getElementsByClassName("number-btn");
@@ -19,36 +21,40 @@ function pressOperatorButtons (event) {
 	}
 }
 
-function pressClearButton (event) {
-	(document.getElementById("clearbtn")).addEventListener ("click", clearData);
-}
-
-function pressEqualsButton (event) {
-	(document.getElementById("equalsbtn")).addEventListener ("click", solveEquation);
-}
+decimalButton.addEventListener ("click", decimalPushToTop);
+(document.getElementById("clearbtn")).addEventListener ("click", clearData);
+(document.getElementById("equalsbtn")).addEventListener ("click", solveEquation);
+(document.getElementById("sqrtbtn")).addEventListener ("click", getSquareRoot);
+(document.getElementById("posnegbtn")).addEventListener ("click", toggleNumberSign);
+(document.getElementById("exponentbtn")).addEventListener ("click", raiseNumberToPower);
 
 function numberPushToTop () {
-	var leftMaxLength = operandLeft.getAttribute("maxlength");
-	var rightMaxLength = operandRight.getAttribute("maxlength");
 	for (var i = 0; i < operatorButtonsArray.length; i++) {
 		operatorButtonsArray[i].removeAttribute("disabled");
 	}
 	if (operatorCenter.value == "") {
 		if (operandLeft.value.length >= leftMaxLength) {
-			operandLeft.value = ((parseInt(operandLeft.value)).toExponential(0)); 
-			operandLeft.value += parseInt(this.value);
+			operandLeft.value = ((parseFloat(operandLeft.value)).toExponential(0)); 
+			operandLeft.value += parseFloat(this.value);
 		} else {
 			operandLeft.value += this.value;
 		}
 	} else {
 		if (operandRight.value.length >= rightMaxLength) {
-			operandRight.value = ((parseInt(operandRight.value)).toExponential(0));
-			operandRight.value += parseInt(this.value);
+			operandRight.value = ((parseFloat(operandRight.value)).toExponential(0));
+			operandRight.value += parseFloat(this.value);
 		} else
 			operandRight.value += this.value;
 	}
 }
 
+function decimalPushToTop () {
+	if (operatorCenter.value == "") {
+		operandLeft.value = parseInt(operandLeft.value) + decimalButton.value; 
+	} else {
+		operandRight.value = parseInt(operandRight.value) + decimalButton.value;
+	}
+}
 
 function checkMaxLength () {
 	operandLeft.getAttribute("maxlength");
@@ -56,7 +62,7 @@ function checkMaxLength () {
 }
 
 function operatorPushToTop () {
-	operatorCenter.value += this.value;
+	operatorCenter.value = this.value;
 }
 
 function clearData () {
@@ -65,25 +71,52 @@ function clearData () {
 	operatorCenter.value = "";
 }
 
+function getSquareRoot () {
+	if (operatorCenter.value == "") {
+		operandLeft.value = (Math.sqrt(operandLeft.value)).toFixed(5);
+	} else {
+		operandRight.value = (Math.sqrt(operandRight.value)).toFixed(5);
+	}
+}
+
+function toggleNumberSign () {
+	if (operatorCenter.value == "") {
+		operandLeft.value *= (-1);
+	} else {
+		operandRight.value *= (-1);
+	}
+}
+
+function raiseNumberToPower () {
+	if (operatorCenter.value == "") {
+		operandLeft.value = Math.pow(operandLeft.value, pressNumberButtons);
+	} else {
+		operandRight.value = Math.pow(operandRight.value, pressNumberButtons);
+	}
+}
+
 function solveEquation () {
-	parseInt(operandLeft.value);
-	parseInt(operandRight.value);
+	var a = operandLeft.value;
+	var b = operandRight.value;
 	var result;
 	if (operatorCenter.value == "+") {
-		result = operandLeft.value + operandRight.value;
+		result = parseFloat(a) + parseFloat(b);
+		console.log(result);
 	} else if (operatorCenter.value == "-") {
-		result = operandLeft.value - operandRight.value;
+		result = parseFloat(a) - parseFloat(b);
 	} else if (operatorCenter.value == "/") {
-		result = operandLeft.value / operandRight.value;
+		result = parseFloat(a) / parseFloat(b);
 	} else if (operatorCenter.value == "*") {
-		result = operandLeft.value * operandRight.value;
+		result = parseFloat(a) * parseFloat(b);
 	}
+	if (((result.toString()).length) > leftMaxLength) {
+		operandLeft.value = result.toFixed(2);
+	} else {
 		operandLeft.value = result;
-		operandRight.value = "";
-		operatorCenter.value = "";
+	}
+	operandRight.value = "";
+	operatorCenter.value = "";
 }
 
 pressNumberButtons(event);
 pressOperatorButtons(event);
-pressClearButton(event);
-pressEqualsButton(event);

773d203ffcb33275f176c59a866399d993a0f5a3
Randi Mays <randimays1024@gmail.com>
2016-07-14 17:32:29 -0500

    Adding ping/pong exercise from PHPIII

---
+++ public/ping.php
@@ -0,0 +1,39 @@
+<?php
+
+	function pageController() {
+
+		$data = [];
+
+		if (isset($_GET["counter"])) {
+			$data["counter"] = $_GET["counter"];
+		} else {
+			$data["counter"] = 0;
+		}
+
+		return $data;
+		
+	}
+
+extract(pageController());
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Counter</title>
+</head>
+<body>
+
+	<h1>Hit Counter: <?= $counter ?></h1>
+
+	<p>
+		<a href="/pong.php?outcome=hit&counter=<?=$counter + 1 ?>">HIT</a>
+	</p>
+	
+	<p>
+		<a href="/pong.php?outcome=miss&counter=<?=$counter = 0 ?>">MISS</a>
+	</p>
+
+</body>
+</html>

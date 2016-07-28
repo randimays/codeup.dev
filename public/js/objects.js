7e259e0579ecca94506b0f95f931a4119efd93f2
Randi Mays <randimays1024@gmail.com>
2016-07-28 08:49:27 -0500

    Updating 3.6 with exercise descriptions

--- public/js/objects.js
+++ public/js/objects.js
@@ -1,25 +1,14 @@
 (function(){
-    "use strict";
+	"use strict";
 
-    // TODO: Create person object
-    var person = {
-    	firstName: 'Randi',
-    	lastName: 'Mays',
-    	sayHello: function() {
-    		return this.firstName + ' ' + this.lastName;
-    	}
-    };
-    
-    alert('Hello, ' + person.sayHello() + '!');
+	// TODO: Create person object with firstName and lastName properties. Add a sayHello method that alerts a greeting using those properties. Say hello from the person object.
+	var person = {
+		firstName: 'Randi',
+		lastName: 'Mays',
+		sayHello: function() {
+			return this.firstName + ' ' + this.lastName;
+		}
+	};
 
 	console.log(person.sayHello() + ' says hello!');
-
-    // TODO: Create firstName and lastName properties in your person object; assign your name to them
-
-
-    // TODO: Add a sayHello method to the person object that
-    //       alerts a greeting using the firstName and lastName properties
-
-    // Say hello from the person object.
-    // person.sayHello();
 })();

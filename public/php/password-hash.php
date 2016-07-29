e4b7fd1288cb041c0450da919c350571252ba827
Randi Mays <randimays1024@gmail.com>
2016-07-29 08:50:06 -0500

    Removing unnecessary lessons

--- public/php/password-hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$plainTextPassword = "1234";
-
-$hashedPassword = password_hash($plainTextPassword, PASSWORD_DEFAULT) . PHP_EOL;
-
-var_dump(password_verify(Input::get("password"), $hashedPassword));
-
